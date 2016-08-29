93cf0887281c12b71e9213bda9a70658b40c470b
Pavel Annin <anninpavel2014@gmail.com>
2016-08-29 16:43:29 +0300

    [Feture] Добавлен SplashScreen

--- app/src/main/kotlin/ru/annin/cryptowallet/presentation/MainActivity.kt
+++ app/src/main/kotlin/ru/annin/cryptowallet/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package ru.annin.cryptowallet
+package ru.annin.cryptowallet.presentation
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import ru.annin.cryptowallet.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/kotlin/ru/annin/cryptowallet/presentation/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package ru.annin.cryptowallet.presentation.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Экран сплеш скрина.
+ *
+ * @author П. Аннин, компания "СофтИнвент" (http://softinvent.ru).
+ */
+class SplashActivity : AppCompatActivity() {
+
+}
